9261a6aebbad0c8500d673e5f716385f35d2e156
FlorianM-C22 <7484@holbertonstudents.com>
2025-09-22 09:41:41 +0200

    Task 3

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -7,6 +7,14 @@ interface Teacher {
   [key: string]: any
 }
 
+interface Directors extends Teacher {
+  numberOfReports: number
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string
+}
+
 const teacher3: Teacher = {
   firstName: "John",
   fullTimeEmployee: false,
@@ -15,4 +23,22 @@ const teacher3: Teacher = {
   contract: false,
 }
 
+const director1: Directors = {
+  firstName: "John",
+  lastName: "Doe",
+  location: "London",
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+}
+
+console.log(director1)
 console.log(teacher3)
+
+const printTeacher: printTeacherFunction = (
+  firstName: string,
+  lastName: string
+): string => {
+  return `${firstName[0]}. ${lastName}`
+}
+
+console.log(printTeacher("John", "Doe"))
