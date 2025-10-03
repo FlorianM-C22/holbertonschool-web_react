b8af5534895af657dd9459fa0f8ae782eea14a48
FlorianM-C22 <7484@holbertonstudents.com>
2025-10-03 10:54:27 +0200

    Task 0

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,9 @@
+const $ = require('jquery')
+
+const PARAGRAPH_DASHBOARD = 'Holberton Dashboard'
+const PARAGRAPH_DATA = 'Dashboard data for the students'
+const PARAGRAPH_COPYRIGHT = 'Copyright - Holberton School'
+
+$('body').append(`<p>${PARAGRAPH_DASHBOARD}</p>`)
+$('body').append(`<p>${PARAGRAPH_DATA}</p>`)
+$('body').append(`<p>${PARAGRAPH_COPYRIGHT}</p>`)
