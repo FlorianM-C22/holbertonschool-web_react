70edc12f841f571bf76b906d05e92f32f5e39a46
FlorianM-C22 <7484@holbertonstudents.com>
2025-10-03 13:13:19 +0200

    Fix updateCounter function

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -3,19 +3,16 @@ import _ from 'lodash'
 
 $('body').append('<p>Holberton Dashboard</p>')
 $('body').append('<p>Dashboard data for the students</p>')
+
 $('body').append('<button>Click here to get started</button>')
 $('body').append('<p id="count">0</p>')
+
 $('body').append('<p>Copyright - Holberton School</p>')
 
 function updateCounter() {
   let count = 0
-  $('button').on(
-    'click',
-    _.debounce(() => {
-      count++
-      $('#count').text(`${count} clicks on the button`)
-    }, 500)
-  )
+  count++
+  $('#count').text(`${count} clicks on the button`)
 }
 
-updateCounter()
+$('button').on('click', _.debounce(updateCounter, 500))
