6bf99c4b8fd962b23a36a3b5afc77f027638f472
FlorianM-C22 <7484@holbertonstudents.com>
2025-10-03 11:02:09 +0200

    Task 0 - Trying to find a fix

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,9 +1,5 @@
 const $ = require('jquery')
 
-const PARAGRAPH_DASHBOARD = 'Holberton Dashboard'
-const PARAGRAPH_DATA = 'Dashboard data for the students'
-const PARAGRAPH_COPYRIGHT = 'Copyright - Holberton School'
-
-$('body').append(`<p>${PARAGRAPH_DASHBOARD}</p>`)
-$('body').append(`<p>${PARAGRAPH_DATA}</p>`)
-$('body').append(`<p>${PARAGRAPH_COPYRIGHT}</p>`)
+$('body').append(`<p>'Holberton Dashboard'</p>`)
+$('body').append(`<p>'Dashboard data for the students'</p>`)
+$('body').append(`<p>'Copyright - Holberton School'</p>`)
