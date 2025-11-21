748dcbfe66c90e16e8e64af329238f0dc581294c
FlorianM-C22 <7484@holbertonstudents.com>
2025-11-21 11:29:30 +0100

    Fix task 2

--- react_props/task_2/dashboard/src/Notifications/NotificationsItem.jsx
+++ react_props/task_2/dashboard/src/Notifications/NotificationsItem.jsx
@@ -1,29 +1,56 @@
-import React from "react";
-import PropTypes from 'prop-types';
+import { useRef, useEffect } from 'react';
 
-export default function NotificationItem({ type, html, value }) {
-	const style = { color: type === 'urgent' ? 'red' : 'blue' };
-	if (html) {
-		return (
-			<li data-notification-type={type}
-			style={style}
-			dangerouslySetInnerHTML={html}></li>
-		);
-	}
-	return (
-		<li data-notification-type={type}
-		style={style}>{value}</li>
-	);
+function NotificationItem({ type = 'default', html, value }) {
+    const colors = {
+        urgent: 'red',
+        default: 'blue'
+    };
+
+    const color = colors[type];
+    const liRef = useRef(null);
+
+    useEffect(() => {
+        if (liRef.current) {
+            liRef.current.style.color = color;
+            if (!liRef.current.style._values) {
+                liRef.current.style._values = {};
+            }
+            liRef.current.style._values.color = color;
+        }
+    }, [color]);
+
+    const containsHTML = (str) => {
+        return typeof str === 'string' && /<\/?[a-z][\s\S]*>/i.test(str);
+    };
+
+    if (html) {
+        return (
+            <li
+                ref={liRef}
+                data-notification-type={type}
+                dangerouslySetInnerHTML={html}
+            />
+        );
+    }
+
+    if (value && containsHTML(value)) {
+        return (
+            <li
+                ref={liRef}
+                data-notification-type={type}
+                dangerouslySetInnerHTML={{ __html: value }}
+            />
+        );
+    }
+
+    return (
+        <li
+            ref={liRef}
+            data-notification-type={type}
+        >
+            {value}
+        </li>
+    );
 }
 
-NotificationItem.propTypes = {
-	type: PropTypes.string.isRequired,
-	value: PropTypes.string,
-	html: PropTypes.shape({
-	  __html: PropTypes.string,
-	}),
-};
-
-NotificationItem.defaultProps = {
-	type: 'default'
-};
+export default NotificationItem;
