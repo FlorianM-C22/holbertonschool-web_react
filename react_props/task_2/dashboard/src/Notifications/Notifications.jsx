bc10ba29bc192e8d7340f402c8250c8e627020ab
FlorianM-C22 <7484@holbertonstudents.com>
2025-11-21 11:47:24 +0100

    Still trying to fix task 2

--- react_props/task_2/dashboard/src/Notifications/Notifications.jsx
+++ react_props/task_2/dashboard/src/Notifications/Notifications.jsx
@@ -1,44 +1,44 @@
 import './Notifications.css'
 import closebtn from '../assets/close-button.png'
-import NotificationItem from './NotificationItem';
+import NotificationItem from './NotificationItem'
 
 export default function Notifications({ notifications }) {
   return (
     <>
       <div className="notifications">
-        <p>
-        Here is the list of notifications
-        </p>
+        <p>Here is the list of notifications</p>
         <button
-        style={{
-          position: "absolute",
-          display: "flex",
-          background: "none",
-          borderStyle: "none",
-          right: "1rem",
-          top: "0.8rem",
-          width: "0.5rem",
-          height: "0.5rem",
-        }}
-        onClick={() => console.log('Close button has been clicked')} aria-label="Close">
-          <img
           style={{
-            width: "0.5rem",
-            height: "0.5rem",
+            position: 'absolute',
+            display: 'flex',
+            background: 'none',
+            borderStyle: 'none',
+            right: '1rem',
+            top: '0.8rem',
+            width: '0.5rem',
+            height: '0.5rem',
           }}
-          src={ closebtn } alt='Close' />
+          onClick={() => console.log('Close button has been clicked')}
+          aria-label="Close"
+        >
+          <img
+            style={{
+              width: '0.5rem',
+              height: '0.5rem',
+            }}
+            src={closebtn}
+            alt="Close"
+          />
         </button>
         <ul>
-          {notifications.map((notification) => (
-            <NotificationItem
-            key={notification.id}
-            type={notification.type}
-            value={notification.value}
-            html={notification.html}
-            />
-          ))}
-          </ul>
+          {notifications.map(notification => {
+            if (notification.html) {
+              return <NotificationItem key={notification.id} type={notification.type} html={notification.html} />
+            }
+            return <NotificationItem key={notification.id} type={notification.type} value={notification.value} />
+          })}
+        </ul>
       </div>
     </>
-  );
+  )
 }
