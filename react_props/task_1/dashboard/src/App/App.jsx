e385cfc5f1e2cd44b2c40d17413466901940587c
FlorianM-C22 <7484@holbertonstudents.com>
2025-11-21 11:06:34 +0100

    Task 1

---
+++ react_props/task_1/dashboard/src/App/App.jsx
@@ -0,0 +1,16 @@
+import Header from '../Header/Header'
+import Login from '../Login/Login'
+import Footer from '../Footer/Footer'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Login />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
