8124a8c81b9a9fb93eab399b46edfbec3d4d8613
FlorianM-C22 <7484@holbertonstudents.com>
2025-11-25 11:20:40 +0100

    BISOUS LE CHECKER

--- react_styling/task_0/dashboard/src/App/App.jsx
+++ react_styling/task_0/dashboard/src/App/App.jsx
@@ -12,27 +12,27 @@ import { getLatestNotification } from '../utils/utils'
 
 class App extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     // This binding is necessary to make `this` work in the callback
-    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this)
   }
 
   static defaultProps = {
-    logOut: () => {}
-  };
+    logOut: () => {},
+  }
 
   componentDidMount() {
-    document.addEventListener("keydown", this.handleKeyDown);
+    document.addEventListener('keydown', this.handleKeyDown)
   }
 
   componentWillUnmount() {
-    document.removeEventListener("keydown", this.handleKeyDown);
+    document.removeEventListener('keydown', this.handleKeyDown)
   }
 
-  handleKeyDown = (event) => {
+  handleKeyDown = event => {
     if (event.ctrlKey && event.key === 'h') {
-      alert('Logging you out');
-      this.props.logOut();
+      alert('Logging you out')
+      this.props.logOut()
     }
   }
 
@@ -41,33 +41,33 @@ class App extends React.Component {
       { id: 1, type: 'default', value: 'New course available' },
       { id: 2, type: 'urgent', value: 'New resume available' },
       { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
-    ];
+    ]
     const coursesList = [
       { id: 1, name: 'ES6', credit: '60' },
       { id: 2, name: 'Webpack', credit: '20' },
       { id: 3, name: 'React', credit: '40' },
-    ];
-    const { isLoggedIn } = this.props;
+    ]
+    const { isLoggedIn } = this.props
 
     return (
       <React.Fragment>
-        <div className='root-notifications'>
+        <div className="root-notifications">
           <Notifications notifications={notificationsList} />
         </div>
-          <Header />
-          {isLoggedIn ? (
-            <BodySectionWithMarginBottom title='Course list'>
-              <CourseList courses={coursesList} />
-            </BodySectionWithMarginBottom>
-          ) : (
-            <BodySectionWithMarginBottom title='Log in to continue'>
-              <Login />
-            </BodySectionWithMarginBottom>
-          )}
-          <BodySection title='News from the School'>
-            <p>Holberton School News goes here</p>
-          </BodySection>
-          <Footer />
+        <Header />
+        {isLoggedIn ? (
+          <BodySectionWithMarginBottom title="Course list">
+            <CourseList courses={coursesList} />
+          </BodySectionWithMarginBottom>
+        ) : (
+          <BodySectionWithMarginBottom title="Log in to continue">
+            <Login />
+          </BodySectionWithMarginBottom>
+        )}
+        <BodySection title="News from the School">
+          <p>Holberton School News goes here</p>
+        </BodySection>
+        <Footer />
       </React.Fragment>
     )
   }
@@ -77,4 +77,4 @@ export default App
 
 App.PropTypes = {
   logOut: PropTypes.func,
-};
+}
